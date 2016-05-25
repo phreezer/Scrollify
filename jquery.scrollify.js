36d66a55e25cc0d0a9a7fcda7814700754bf06ab
phreezer <marcus.showalter@gmail.com>
2016-05-25 12:19:04 -0700

    Improvements with Angular JS Compatibility

--- jquery.scrollify.js
+++ jquery.scrollify.js
@@ -14,10 +14,10 @@
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
  * the Software, and to permit persons to whom the Software is furnished to do so,
  * subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -27,33 +27,33 @@
  */
 (function (global,factory) {
 	"use strict";
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['jquery'], function($) {
-        	return factory($, global, global.document);
-        });
-    } else if (typeof module === 'object' && module.exports) {
-        // Node/CommonJS
-        module.exports = function( root, jQuery ) {
-            if ( jQuery === undefined ) {
-                // require('jQuery') returns a factory that requires window to
-                // build a jQuery instance, we normalize how we use modules
-                // that require this pattern but the window provided is a noop
-                // if it's defined (how jquery works)
-                if ( typeof window !== 'undefined' ) {
-                    jQuery = require('jquery');
-                }
-                else {
-                    jQuery = require('jquery')(root);
-                }
-            }
-            factory(jQuery, global, global.document);
-            return jQuery;
-        };
-    } else {
-        // Browser globals
-        factory(jQuery, global, global.document);
-    }
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['jquery'], function($) {
+			return factory($, global, global.document);
+		});
+	} else if (typeof module === 'object' && module.exports) {
+		// Node/CommonJS
+		module.exports = function( root, jQuery ) {
+			if ( jQuery === undefined ) {
+				// require('jQuery') returns a factory that requires window to
+				// build a jQuery instance, we normalize how we use modules
+				// that require this pattern but the window provided is a noop
+				// if it's defined (how jquery works)
+				if ( typeof window !== 'undefined' ) {
+					jQuery = require('jquery');
+				}
+				else {
+					jQuery = require('jquery')(root);
+				}
+			}
+			factory(jQuery, global, global.document);
+			return jQuery;
+		};
+	} else {
+		// Browser globals
+		factory(jQuery, global, global.document);
+	}
 }(typeof window !== 'undefined' ? window : this, function ($, window, document, undefined) {
 	"use strict";
 	var heights = [],
@@ -92,18 +92,63 @@
 			after:function() {},
 			afterResize:function() {},
 			afterRender:function() {}
-		};
+		},
+		currentSectionID, // Angular Fix
+		isIE;
+	
+	function isIECheck(){
+		
+		var ua = window.navigator.userAgent;
+		var msie = ua.indexOf('MSIE ');
+		var version;
+		if (msie > 0) {
+			// IE 10 or older => return version number
+			version = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+		}
+
+		var trident = ua.indexOf('Trident/');
+		if (trident > 0) {
+			// IE 11 => return version number
+			var rv = ua.indexOf('rv:');
+			version = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+		}
+		if(version) {
+			isIE = true;
+		} else {
+			isIE = false;
+		}
+		return isIE;
+	}
+	
 	function animateScroll(index,instant,callbacks) {
 		if(disabled===true) {
 			return true;
 		}
 		if(names[index]) {
 			scrollable = false;
+			
+			//console.log('CURRENT:', index, elements.length, elements[index], elements, heights);
+			if(index === 0) {
+				$('.btn-prev').addClass('hide');
+			} else {
+				$('.btn-prev').removeClass('hide');
+			}
+			if(index+1 >= elements.length) {
+				$('.btn-next').addClass('hide');
+			} else {
+				$('.btn-next').removeClass('hide');
+			}
+			$('section').removeClass('active-section');
+			$(elements[index]).addClass('active-section');
+			
 			if(callbacks) {
 				settings.before(index,elements);
 			}
 			interstitialIndex = 1;
-			if(settings.sectionName && !(firstLoad===true && index===0)) {
+			if(!settings.sectionName && !(firstLoad===true && index===0)) {
+
+				currentSectionID = names[index];		// Angular Fix
+				/*
 				if(history.pushState) {
 				    try {
 						history.replaceState(null, null, names[index]);
@@ -112,10 +157,11 @@
 				    		console.warn("Scrollify warning: This needs to be hosted on a server to manipulate the hash value.");
 				    	}
 				    }
-				    
+
 				} else {
 					window.location.hash = names[index];
 				}
+				*/
 			}
 			if(instant) {
 				$(settings.target).stop().scrollTop(heights[index]);
@@ -123,32 +169,43 @@
 					settings.after(index,elements);
 				}
 			} else {
-				locked = true;
-				$(settings.target).stop().animate({
-					scrollTop: heights[index]
-				}, {
-					step: function() {
-						if(!scrolled) {
-							$(settings.target).stop();
+				if(firstLoad===true && index===0) {
+					locked = false;
+					firstLoad = false;
+					scrolled = true;
+				} else {
+					locked = true;
+					$(settings.target).stop().animate({
+						scrollTop: heights[index]
+					}, {
+						step: function() {
+							if(!scrolled) {
+								$(settings.target).stop();
+							}
+						},
+						duration: settings.scrollSpeed,
+						easing: settings.easing
+					});
+					$(settings.target).promise().done(function(){
+						locked = false;
+						firstLoad = false;
+						scrolled = true;
+						if(callbacks) {
+							settings.after(index,elements);
 						}
-					},
-					duration: settings.scrollSpeed,
-					easing: settings.easing
-				});
+					});
+				}
+				
 
+
+				/* Angular Fix
 				if(window.location.hash.length) {
 					if($(window.location.hash).length && window.console) {
 						console.warn("Scrollify warning: There are IDs on the page that match the hash value - this will cause the page to anchor.");
 					}
 				}
-				$(settings.target).promise().done(function(){
-					locked = false;
-					firstLoad = false;
-					scrolled = true;
-					if(callbacks) {
-						settings.after(index,elements);
-					}
-				});
+				*/
+
 			}
 
 		}
@@ -156,38 +213,62 @@
 
 	function isAccelerating(samples) {
 
-        if(samples<4) {
-        	return false;
-        }
-        var limit = 20,sum = 0,i = samples.length-1,l;
-        if(samples.length<limit*2) {
-        	limit = Math.floor(samples.length/2);
-        }
-        l = samples.length-limit;
-        for(;i>=l;i--) {
-        	sum = sum+samples[i];
-        }
-        var average1 = sum/limit;
+		if(samples<4) {
+			return false;
+		}
+		var limit = 20,sum = 0,i = samples.length-1,l;
+		if(samples.length<limit*2) {
+			limit = Math.floor(samples.length/2);
+		}
+		l = samples.length-limit;
+		for(;i>=l;i--) {
+			sum = sum+samples[i];
+		}
+		var average1 = sum/limit;
 
-        sum = 0;
-        i = samples.length-limit-1;
-        l = samples.length-(limit*2);
-        for(;i>=l;i--) {
-        	sum = sum+samples[i];
-        }
-        var average2 = sum/limit;
+		sum = 0;
+		i = samples.length-limit-1;
+		l = samples.length-(limit*2);
+		for(;i>=l;i--) {
+			sum = sum+samples[i];
+		}
+		var average2 = sum/limit;
 
-        if(average1>=average2) {
-        	return true;
-        } else {
-        	return false;
-        }
+		if(average1>=average2) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	$.scrollify = function(options) {
 		$.easing['easeOutExpo'] = function(x, t, b, c, d) {
 			return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
 		};
+
+		$.easing['easeOutCubic'] = function (x, t, b, c, d) {
+			return c*((t=t/d-1)*t*t + 1) + b;
+		};
 		
+		$.easing['easeInOutQuad'] = function (x, t, b, c, d) {
+			if ((t/=d/2) < 1) return c/2*t*t + b;
+			return -c/2 * ((--t)*(t-2) - 1) + b;
+		};
+		
+		$.easing['easeInOutCubic'] = function (x, t, b, c, d) {
+			if ((t/=d/2) < 1) return c/2*t*t*t + b;
+			return c/2*((t-=2)*t*t + 2) + b;
+		};
+		
+		$.easing['easeInOutSine'] = function (x, t, b, c, d) {
+			return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+		};
+		
+		$.easing['easeInOutExpo'] = function (x, t, b, c, d) {
+			if (t==0) return b;
+			if (t==d) return b+c;
+			if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+			return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+		};
 
 		manualScroll = {
 			handleMousedown:function() {
@@ -196,26 +277,36 @@
 				}
 				scrollable = false;
 				scrolled = false;
+				if(isIE) {
+					$(document).bind('mousemove',function(event){
+						if(event.pageX < ($(window).width() - 30)){
+							//mouse is off scrollbar
+							$(this).unbind(event);
+							$(this).trigger('mouseup');
+						}
+					});
+				}
 			},
 			handleMouseup:function() {
 				if(disabled===true) {
 					return true;
 				}
 				scrollable = true;
+				/*
 				if(scrolled) {
 					manualScroll.calculateNearest();
-				}
+				}*/
 				scrolled = true;
+				manualScroll.calculateNearest();
 			},
 			handleScroll:function() {
 				if(disabled===true) {
 					return true;
 				}
 				if(timeoutId){
-					clearTimeout(timeoutId);  
+					clearTimeout(timeoutId);
 				}
 				timeoutId = setTimeout(function(){
-						
 					scrolled = true;
 					if(scrollable===false) {
 						return false;
@@ -234,7 +325,7 @@
 					diff;
 				for(;i<max;i++) {
 					diff = Math.abs(heights[i] - top);
-					
+
 					if(diff < prev) {
 						prev = diff;
 						closest = i;
@@ -259,7 +350,7 @@
 				var currentScrollTime = new Date().getTime();
 				delta = delta || -e.originalEvent.detail / 3 || e.originalEvent.wheelDelta / 120;
 
-				
+
 				if((currentScrollTime-scrollTime) > 1300){
 					scrollSamples = [];
 				}
@@ -324,6 +415,8 @@
 				}
 			},
 			init:function() {
+				
+				isIECheck();
 				if(settings.scrollbars) {
 					$(window).bind('mousedown', manualScroll.handleMousedown);
 					$(window).bind('mouseup', manualScroll.handleMouseup);
@@ -331,15 +424,15 @@
 				} else {
 					$("body").css({"overflow":"hidden"});
 				}
-				
+
 				$(document).bind('DOMMouseScroll mousewheel',manualScroll.wheelHandler);
 				$(document).bind('keydown', manualScroll.keyHandler);
 			}
 		};
-		
+
 		swipeScroll = {
 			touches : {
-				"touchstart": {"y":-1,"x":-1}, 
+				"touchstart": {"y":-1,"x":-1},
 				"touchmove" : {"y":-1,"x":-1},
 				"touchend"  : false,
 				"direction" : "undetermined"
@@ -358,7 +451,7 @@
 					}
 				}
 				var touch;
-				if (typeof event !== 'undefined'){	
+				if (typeof event !== 'undefined'){
 					if (typeof event.touches !== 'undefined') {
 						touch = event.touches[0];
 						switch (event.type) {
@@ -376,22 +469,22 @@
 								swipeScroll.touches.touchmove.x = touch.pageX;
 								if(swipeScroll.touches.touchstart.y!==swipeScroll.touches.touchmove.y && (Math.abs(swipeScroll.touches.touchstart.y-swipeScroll.touches.touchmove.y)>Math.abs(swipeScroll.touches.touchstart.x-swipeScroll.touches.touchmove.x))) {
 									//if(!overflow[index]) {
-										event.preventDefault();
+									event.preventDefault();
 									//}
 									swipeScroll.touches.direction = "y";
 									if((swipeScroll.options.timeStamp+swipeScroll.options.timeGap)<(new Date().getTime()) && swipeScroll.touches.touchend == false) {
-										
+
 										swipeScroll.touches.touchend = true;
 										if (swipeScroll.touches.touchstart.y > -1) {
 
 											if(Math.abs(swipeScroll.touches.touchmove.y-swipeScroll.touches.touchstart.y)>swipeScroll.options.distance) {
 												if(swipeScroll.touches.touchstart.y < swipeScroll.touches.touchmove.y) {
-													
+
 													swipeScroll.up();
 
 												} else {
 													swipeScroll.down();
-													
+
 												}
 											}
 										}
@@ -406,10 +499,10 @@
 										if(Math.abs(swipeScroll.touches.touchmove.y-swipeScroll.touches.touchstart.y)>swipeScroll.options.distance) {
 											if(swipeScroll.touches.touchstart.y < swipeScroll.touches.touchmove.y) {
 												swipeScroll.up();
-												
+
 											} else {
 												swipeScroll.down();
-												
+
 											}
 										}
 										swipeScroll.touches.touchstart.y = -1;
@@ -426,7 +519,7 @@
 			down: function() {
 				if(index<=heights.length-1) {
 					if(atBottom() && index<heights.length-1) {
-						
+
 						index++;
 						animateScroll(index,false,true);
 					} else {
@@ -438,23 +531,23 @@
 						} else {
 							interstitialScroll(parseInt(heights[index])+(elements[index].height()-$(window).height()));
 						}
-						
+
 					}
 				}
 			},
 			up: function() {
 				if(index>=0) {
 					if(atTop() && index>0) {
-						
+
 						index--;
 						animateScroll(index,false,true);
 					} else {
-						
+
 						if(interstitialIndex>2) {
 
 							interstitialIndex -= 1;
 							interstitialScroll(parseInt(heights[index])+($(window).height()*interstitialIndex));
-							
+
 						} else {
 
 							interstitialIndex = 1;
@@ -466,8 +559,8 @@
 			},
 			init: function() {
 				if (document.addEventListener) {
-					document.addEventListener('touchstart', swipeScroll.touchHandler, false);	
-					document.addEventListener('touchmove', swipeScroll.touchHandler, false);	
+					document.addEventListener('touchstart', swipeScroll.touchHandler, false);
+					document.addEventListener('touchmove', swipeScroll.touchHandler, false);
 					document.addEventListener('touchend', swipeScroll.touchHandler, false);
 				}
 			}
@@ -485,7 +578,7 @@
 			}
 		};
 		settings = $.extend(settings, options);
-		
+
 		sizePanels();
 
 		calculatePositions(false);
@@ -495,7 +588,7 @@
 		} else {
 			animateScroll(0,true,true);
 		}
-		
+
 		manualScroll.init();
 		swipeScroll.init();
 
@@ -515,11 +608,11 @@
 				if(settings.setHeights) {
 					if($(this).css("height","auto").outerHeight()<$(window).height()) {
 						$(this).css({"height":$(window).height()});
-						
+
 						overflow[i] = false;
 					} else {
 						$(this).css({"height":$(this).height()});
-						
+
 						overflow[i] = true;
 					}
 				} else {
@@ -543,19 +636,24 @@
 				} else {
 					names[i] = "#" + (i + 1);
 				}
-				
+
 				elements[i] = $(this);
 
 				if($(names[i]).length && window.console) {
 					console.warn("Scrollify warning: Section names can't match IDs on the page - this will cause the browser to anchor.");
 				}
+				/*
 				if(window.location.hash===names[i]) {
 					index = i;
 					hasLocation = true;
-					
+				}*/
+				if(currentSectionID===names[i]) {		// Angular Fix
+					index = i;
+					hasLocation = true;
 				}
+
 			});
-			
+
 			if(true===resize) {
 				animateScroll(index,false,false);
 			} else {
@@ -647,14 +745,17 @@
 		$(document).unbind('keydown', manualScroll.keyHandler);
 
 		if (document.addEventListener) {
-			document.removeEventListener('touchstart', swipeScroll.touchHandler, false);	
-			document.removeEventListener('touchmove', swipeScroll.touchHandler, false);	
+			document.removeEventListener('touchstart', swipeScroll.touchHandler, false);
+			document.removeEventListener('touchmove', swipeScroll.touchHandler, false);
 			document.removeEventListener('touchend', swipeScroll.touchHandler, false);
 		}
 		heights = [];
 		names = [];
 		elements = [];
 		overflow = [];
+		currentSectionID = null;
+		firstLoad = true;
+		index = 0;
 	};
 	$.scrollify.update = function() {
 		util.handleResize();
